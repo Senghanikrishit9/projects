5ca0b5f129acfcdce8aa745b388b6d497baa5bc9
krishit senghani <krishitsenghani22@gmail.com>
2026-02-05 13:37:42 +0530

    Nested scope

--- krishit.js
+++ krishit.js
@@ -1,25 +1,20 @@
-const x = 100;
-if(true){
-  const y = 200
+// Nested scope
+function first() {
+  const x = 100;
+
+  function second() {
+    const y = 200;
+    console.log(x + y);
+  }
+
+  second();
+}
+first();
+
+if (true) {
+  const x = 100;
+
+  if (x === 100);
+  const y = 500;
   console.log(x + y);
 }
-// console.log(x+y) = can't access because its cannot declare in global varible
-
-for (let i=0;i<=10;i++){
-  console.log(i);
-}
-
-if(true){
-  const a = 500;
-  let b = 600;
-  var c = 900;
-}
-
-console.log(c);
-
-function run(){
-  var d = 100;
-  console.log(d);
-}
-
-run();
