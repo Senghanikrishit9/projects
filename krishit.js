2452edd331e6ba43b8761a06d288ae7a36576b8c
krishit senghani <krishitsenghani22@gmail.com>
2026-02-07 17:58:15 +0530

    loops

--- krishit.js
+++ krishit.js
@@ -1,12 +1,21 @@
-// Exection Context
-let x = 100
-let y = 50 
-function getSum(n1,n2){
-  let sum = n1 + n2;
-  return sum;
+if(true){
+  console.log("you are my frnd");
 }
-const sum1 = getSum(x,y);
-const sum2 =getSum(10,5);
 
-console.log(sum1,sum2);
+if(true){
+  console.log("this is a true statment");
+}
 
+const x = 10;
+const y = 9;
+if(x>y){
+  console.log(`${x} is greater then ${y}`);
+}
+
+let a = 23;
+let b = 23;
+if(a!=b){
+  console.log(`${a} is not equal to ${b}`);
+}else{
+  console.log(`${a} is equal to ${b}`);
+}
