b87c9c1462be27f87381471c471f63235ba79156
krishit senghani <krishitsenghani22@gmail.com>
2026-02-08 18:06:16 +0530

    emial

--- krishit.js
+++ krishit.js
@@ -1,25 +1,28 @@
-function calculator (num1,num2,operator){
- let result;
+const emial = "krishitsenghani@22gmail.com";
 
-
-switch(operator){
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    case '/':
-        result = num1 / num2;
-        break;
-    default :
-       result = 'Invalid Syntax';
-}
-  console.log(result);
-  return result;
+if (emial) {
+  console.log("you enter your email");
 }
 
-calculator(34,45,'=');
+const x = false;
+
+if (x) {
+  console.log("this is a turthy");
+} else {
+  console.log("this is a flasy");
+}
+console.log(Boolean(x));
+
+const children = 0;
+if (children !== undefined) {
+  console.log(`you have ${children} numbers of children`);
+} else {
+  console.log("Please enter you children ");
+}
+
+const post = ["post"];
+if (post.length > 0) {
+  console.log("list posts");
+} else {
+  console.log("No post to list");
+}
