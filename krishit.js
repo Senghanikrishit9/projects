ef7395cd41fd4f4d366473c9171299c43a68d933
krishit senghani <krishitsenghani22@gmail.com>
2026-02-10 19:54:45 +0530

    hello

--- krishit.js
+++ krishit.js
@@ -1,26 +1,11 @@
-// 1. ||= assigns the right side value only if the left is a flase value.
-let a = false;
-
-if(!a){
-  a = 10;
+// For loop
+for (let i = 0; i < 5;i ++){
+  console.log("Number" + i)
 }
 
-a ||= 10;
-console.log(a);
-// 2. &&= assigns the right side value only if the left value is a turthy.
-
-let b = 10;
-
-if(b){
-  b = 20;
-}
-
-b = b && 20;
-console.log(b); 
-// 3. ??= assigns the right  side value only if the left is null undefined.
-let c = null;
-
-if(c === null || c === undefined){
-  c = 30;
-}
-console.log(c);
+  for (let i = 0; i <=10;i ++){
+    if(i === 7){
+      console.log("7 is my luchy number")
+    }
+  console.log("Number" + i)
+  }
