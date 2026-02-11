f11ea318199a9e6f1c24e6af680a929fc2811ac5
krishit senghani <krishitsenghani22@gmail.com>
2026-02-11 17:27:22 +0530

    hello

--- krishit.js
+++ krishit.js
@@ -1,17 +1,10 @@
-// break loop
-// for(let i=0;i<=30;i++){
-//   if(i===20){
-//     console.log("loops is break");
-//     break;
-//   };
-//   console.log(i);
-// };
-
-//continue loop 
-for (let i =0;i<=20;i++){
-  if(i===13){
-    console.log("skipped the 13");
-    continue;
-  }
-  console.log(i);
+// while loop
+let i = 0;
+ 
+while(i<=20){
+  console.log("number" + i);
+  i++;
 }
+
+
+
