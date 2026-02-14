ee3136206667d00bb4ef206537c72bd093efc49f
krishit senghani <krishitsenghani22@gmail.com>
2026-02-14 15:15:20 +0530

    hello

--- krishit.js
+++ krishit.js
@@ -1,8 +1,20 @@
-let i =0;
+// 1. Array filters
+const word = ['krishit','krish','megh'];
 
-for(let i=0;i<100;i++){
-    if(i *= 3){
-        console.log("fizz");
-    }
-    console.log(i)
-}
+const result =word.filter(word => word.length >3)
+   console.log(result);
+
+
+// 2. Long version
+let number = [1,2,3,4,5,6,7,8,9,10];
+
+const output = number.filter(function (number){
+  return number %  2 === 0;
+})
+
+console.log(output);
+
+// 3.Short version
+
+const outputs = number.filter(number => number %  3 === 0);
+   console.log(outputs);
