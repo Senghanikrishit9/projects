b5c91e25dc592ad3ea661ea9ec7a0447c6505000
krishit senghani <krishitsenghani22@gmail.com>
2026-02-16 10:53:28 +0530

    hello

--- krishit.js
+++ krishit.js
@@ -1,27 +1,54 @@
-// Array.map Method 
-const Array1 =[10,20,30,40];
+// Array challange
 
-const output = Array1.map(Array1 => Array1 + 2);
-console.log(output);
+// challange first
 
-
-const nav = [23,45,67,89,90];
-
-const out = nav.map(nav => 'nav' + nav);
-console.log(nav);
-
-
-
-
-const company = {
-   name : "tata",
-   categrios : "nanoo"
-};
+const people = [
+  {
+    firstname: "krishit",
+    lastname: "senghani",
+    email: "krishitsenghani22@gmail.com",
+    Phonenum: 7383623637,
+    Age: 19,
+  },
+  {
+    firstname: "krish",
+    lastname: "senghani",
+    email: "krishsenghani79@gmail.com",
+    Phonenum: 7433983536,
+    Age: 19,
+  },
+  {
+    firstname: "Adrash",
+    lastname: "senghani",
+    email: "Adrashsenghani84@gmail.com",
+    Phonenum: 8401672436,
+    Age: 56,
+  },
+  {
+    firstname: "jay",
+    lastname: "senghani",
+    email: "jaysenghani23@gmail.com",
+    Phonenum: 8645893409,
+    Age: 67,
+  },
+];
  
-const comapnies = company.map((company) => {
-   return{
-      name : company.name,
-      categrios : company.categrios
-   }
-});
-console.log(comapnies);
+const youngpeople  = people.filter((people) => people.Age <= 25)
+console.log(youngpeople);
+
+// challage second
+
+const number = [2,-30,50,20,12,-9,7]
+const postivenum = number.filter((number) => number > 0)
+console.log(postivenum);
+const nagativenum = number.filter((number => number < 0))
+console.log(nagativenum);
+
+// change third 
+
+const words = ['coders','programmer','developer'];
+
+const cwords = words.map((words) =>{
+   return words[0].toUpperCase() + words.slice();
+})
+console.log(cwords);
