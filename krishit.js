454f42275cf08236b8136305fe90e422a801fed6
krishit senghani <krishitsenghani22@gmail.com>
2026-02-23 18:12:00 +0530

    hello

--- krishit.js
+++ krishit.js
@@ -1,54 +1,10 @@
-// Array challange
+function insertAfter(newEl,existingEl){
+    existingEl.parentElments.insertbefore(newEl,existingEl.nextSibling);
+}
 
-// challange first
+const li = document.querySelector("li");
+li.textContent = 'Insert Me after'
 
-const people = [
-  {
-    firstname: "krishit",
-    lastname: "senghani",
-    email: "krishitsenghani22@gmail.com",
-    Phonenum: 7383623637,
-    Age: 19,
-  },
-  {
-    firstname: "krish",
-    lastname: "senghani",
-    email: "krishsenghani79@gmail.com",
-    Phonenum: 7433983536,
-    Age: 19,
-  },
-  {
-    firstname: "Adrash",
-    lastname: "senghani",
-    email: "Adrashsenghani84@gmail.com",
-    Phonenum: 8401672436,
-    Age: 56,
-  },
-  {
-    firstname: "jay",
-    lastname: "senghani",
-    email: "jaysenghani23@gmail.com",
-    Phonenum: 8645893409,
-    Age: 67,
-  },
-];
- 
-const youngpeople  = people.filter((people) => people.Age <= 25)
-console.log(youngpeople);
+const firstItem = document.querySelector('li : first-child');
 
-// challage second
-
-const number = [2,-30,50,20,12,-9,7]
-const postivenum = number.filter((number) => number > 0)
-console.log(postivenum);
-const nagativenum = number.filter((number => number < 0))
-console.log(nagativenum);
-
-// change third 
-
-const words = ['coders','programmer','developer'];
-
-const cwords = words.map((words) =>{
-   return words[0].toUpperCase() + words.slice();
-})
-console.log(cwords);
+insertAfter(li,firstItem);
