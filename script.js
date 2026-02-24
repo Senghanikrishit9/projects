88b35aaaa2a6506683a827f42061c3256e8dd900
krishit senghani <krishitsenghani22@gmail.com>
2026-02-24 19:54:16 +0530

    hello

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-function insertAfter(newEl,existingEl){
-    existingEl.parentElments.insertbefore(newEl,existingEl.nextSibling);
+function replaceFirstItems() {
+  const fristItem = document.querySelector("li:first-child");
+
+  const li = document.createElement("li");
+  li.textContent = "jalebi";
+
+  fristItem.replaceWith(li);
 }
 
-const li = document.querySelector("li");
-li.textContent = 'Insert Me after'
+function replacesecondItem() {
+  const secondItem = document.querySelector("li:second-child");
 
-const firstItem = document.querySelector('li : first-child');
+  const li = document.createElement("li");
+  li.textContent = "krishit";
+  secondItem.replaceWith(li);
+}
 
-insertAfter(li,firstItem);
+function replaceAllitems() {
+  const replaceAll = document.querySelectorAll("li");
+
+  lis.forEach((items) => {
+    items.outerHtml = "<li> Replace All items </li>";
+  });
+}
+
+function replaceAllitem() {
+  const items = document.querySelectorAll("li");
+  li.forEach(items, (index) => {
+    lis.innerHtml = "raplace All the items";
+  });
+}
+let h1 = document.querySelector("h1");
+function replaceHeading() {
+  h1.textContent = "krishit";
+}
+replaceHeading();
+
+replaceFirstItems();
+replacesecondItem();
+replaceAllitems();
+replaceAllitem();
