ce64276d19756fbdb4b5d3625dc9d039b51e24e2
krishit senghani <krishitsenghani22@gmail.com>
2026-02-25 14:08:35 +0530

    feature

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-function replaceFirstItems() {
-  const fristItem = document.querySelector("li:first-child");
+const text = document.querySelector("p");
+const itemlist = document.querySelector(".item-list-items");
+const items = itemlist.querySelectorAll("li");
 
-  const li = document.createElement("li");
-  li.textContent = "jalebi";
-
-  fristItem.replaceWith(li);
-}
-
-function replacesecondItem() {
-  const secondItem = document.querySelector("li:second-child");
-
-  const li = document.createElement("li");
-  li.textContent = "krishit";
-  secondItem.replaceWith(li);
-}
-
-function replaceAllitems() {
-  const replaceAll = document.querySelectorAll("li");
-
-  lis.forEach((items) => {
-    items.outerHtml = "<li> Replace All items </li>";
+function run(){
+  console.log(itemlist.className);
+  text.className = "dark";
+  items.forEach((item,index) => {
+    itemlist.style.color = 'red';
+    
+    if(index === 2){
+      itemlist.style.color = 'blue';
+    }
   });
 }
 
-function replaceAllitem() {
-  const items = document.querySelectorAll("li");
-  li.forEach(items, (index) => {
-    lis.innerHtml = "raplace All the items";
-  });
-}
-let h1 = document.querySelector("h1");
-function replaceHeading() {
-  h1.textContent = "krishit";
-}
-replaceHeading();
-
-replaceFirstItems();
-replacesecondItem();
-replaceAllitems();
-replaceAllitem();
+document.querySelector('button').onclick = run;
