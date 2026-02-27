8265fb7582f3fafd7ada4db688bef0ee054385a0
krishit senghani <krishitsenghani22@gmail.com>
2026-02-27 20:51:39 +0530

    Events

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-const text = document.querySelector("p");
-const itemlist = document.querySelector(".item-list-items");
-const items = itemlist.querySelectorAll("li");
-
-function run(){
-  console.log(itemlist.className);
-  text.className = "dark";
-  items.forEach((item,index) => {
-    itemlist.style.color = 'red';
-    
-    if(index === 2){
-      itemlist.style.color = 'blue';
-    }
-  });
-}
-
-document.querySelector('button').onclick = run;
+const btn = document.querySelector("button");
+btn.addEventListener("click", () =>{
+  console.log("hello my child");
+  document.querySelector("body").style.background = "blue";
+})
