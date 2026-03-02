2520a8926cb407b4cc8e0b3a09a396da221a83d1
krishit senghani <krishitsenghani22@gmail.com>
2026-03-02 17:06:38 +0530

    keyboards events

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const btn = document.querySelector(".btn")
-// 1.Method 
-btn.addEventListener("click", () => {
-  console.log("button is cliked")
-});
-// 2. Method 
-btn.addEventListener("click",function (){
-  console.log("button was clicked");
-});
+// keyboards events
+const btn = document.querySelector(".btn");
+ btn.addEventListener("click",() => {
+  console.log("key press");
+ })
+
+ const btn1 = document.querySelector(".btn1");
+ btn.addEventListener("click",() => {
+  console.log("key up");
+ });
+
+
