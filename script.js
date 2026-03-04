f05490120659ac98013e73b541f371a635666ce1
krishit senghani <krishitsenghani22@gmail.com>
2026-03-04 18:51:06 +0530

    events

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// keyboards events
-const btn = document.querySelector(".btn");
- btn.addEventListener("click",() => {
-  console.log("key press");
- })
+window.addEventListener('keypress', (e) => {
+  const insert = document.getElementById('insert');
 
- const btn1 = document.querySelector(".btn1");
- btn.addEventListener("click",() => {
-  console.log("key up");
- });
+  insert.innerHTML = `
+    <div class="key">
+        ${e.key}
+        <small>e.key</small>
+      </div>
 
-
+      <div class="key">
+        ${e.keyCode}
+        <small>e.keyCode</small>
+      </div>
+      <div class="key">
+        ${e.code}
+        <small>e.code</small>
+      </div>
+    `;
+});
