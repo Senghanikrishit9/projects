2d9ddfc56111b772207572614a87ee58847011bc
krishit senghani <krishitsenghani22@gmail.com>
2026-03-17 09:27:44 +0530

    joke generator

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// http requests 
+const jokeEl = document.getElementById("joke");
+const jokebtn = document.getElementById("joke-btn");
 
-const xhr = new XMLHttpRequest();
+const generatejoke = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
 
-xhr.open('Get','./movies.json');
-
-xhr.onreadystatechange = function() {
- if(this.readyState === 4 && this.status === 200){
-  console.log(JSON.parse(this.responseText));
- }
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        jokeEl.innerHTML = JSON.parse(this.responseText).value;
+      } else {
+        jokeEl.innerHTML = "something went worng";
+      }
+    }
+  };
+  xhr.send();
 };
 
-xhr.send();
+jokebtn.addEventListener("click", generatejoke);
+document.addEventListener('DOMContentLoaded',generatejoke);
