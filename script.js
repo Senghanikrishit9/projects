a6b1b795fd4331e06d83f5c43afe7c540303dd60
krishit senghani <krishitsenghani22@gmail.com>
2026-03-18 11:44:39 +0530

    promise && callbacks

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const jokeEl = document.getElementById("joke");
-const jokebtn = document.getElementById("joke-btn");
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-const generatejoke = () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+    xhr.open("GET", endpoint);
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        jokeEl.innerHTML = JSON.parse(this.responseText).value;
-      } else {
-        jokeEl.innerHTML = "something went worng";
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject('something went wrong');
+        }
       }
-    }
-  };
-  xhr.send();
-};
+    };
 
-jokebtn.addEventListener("click", generatejoke);
-document.addEventListener('DOMContentLoaded',generatejoke);
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000);
+  });
+}
+
+getData('./movies.json')
+  .then((movie) => {
+    console.log(movie);
+    return getData('./actor.json'); 
+  })
+  .then((actor) => {
+    console.log(actor);
+    return getData('./director.json'); 
+  })
+  .then((director) => {
+    console.log(director);
+  })
+  .catch((error) => console.log(error));
