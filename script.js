1ecae427f5ee33ae32bc95f294dd8083a090b166
krishit senghani <krishitsenghani22@gmail.com>
2026-03-23 20:18:32 +0530

    typecode

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-function createPost({ title, body }) {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            body
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-            token: 'abc123'
-        }
-    }).then((res) => res.json())
-        .then((data) => console.log(data))
-};
+const apiurl = 'https://jsonplaceholder.typicode.com/todos';
 
-createPost({ title: 'My Post', body: 'This is my body' });
+const getodo = () =>{
+    fetch(apiurl + '?_limit=5')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach((todo) => {
+          const div = document.createElement('div')
+          div.appendChild(document.createTextNode(todo.title));
 
-
-function createPost1({ title, body }) {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POSt',
-        body: JSON.stringify({  
-            title,
-            body
-        }),
-        headers: {
-            'content-type': 'application/json',
-            token: 'abc123'
-        }
-    }).then((res) => res.json())
-        .then((data) => console.log(data));
+          document.getElementById('todo').appendChild(div);
+        });
+    })
 }
 
-createPost1({ title: "my post 1", body: 'this is my second body' });
+getodo();
