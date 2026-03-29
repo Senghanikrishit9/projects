ccccbd861ff7cdad6005a5073c6291a7fac75d2d
krishit senghani <krishitsenghani22@gmail.com>
2026-03-29 10:03:25 +0530

    multiple promise

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
-const getUsers = async () => {
-    try{
-    const response = await fetch ('http://jsonplaceholder.typicode.com/users');
-    
+function getData(endpoint) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('Get', endpoint);
 
-    const data = await response.text();
-    console.log(data);
-    }catch(error){
-     console.log(" Is not working " + error)
-    }
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject('Error :  something went wrong');
+                }
+            }
+        };
+
+        setTimeout(() => {
+            xhr.send();
+        }, Math.floor(Math.random() * 3000) + 1000);
+
+    });
 }
 
 
+getData("./movie.json")
+    .then((movies) => {
+        console.log(movies)
+    })
+getData("./acotor.json")
+    .then((acotor) => {
+        console.log(acotor)
+    });
+getData("./director.json")
+    .then((directors) => {
+        console.log(directors);
+    })
+    .catch((error) => console.log(error));
 
-getUsers();
+async function getAlldata() {
+    const movies = await getData('./movie.json')
+    console.log(movies);
+    const acotor = await getData('./acotor.json')
+    console.log(acotor);
+    const director = await getData('./director.json')
+    console.log(director);
+}
+getAlldata();
+
+async function getAlldatawithfecth() {
+    const moviesres = await fetch('./movie.json')
+    const movie = await moviesres.json();
+    const directorres = await fetch('./director.json')
+    const director = await directorres.json();
+    const acotorres = await fetch('./acotor.json')
+    const acotor = await acotorres.json();
+
+    console.log(movie, director, acotor);
+}
+
+getAlldatawithfecth();
+
+async function getAlldatawithPromiseAll() {
+    const [moviesres, directorres, acotorres] = await Promise.all([
+        fetch('./movie.json'),
+        fetch('./acotor.json'),
+        fetch('./director.json')
+    ]);
+
+    const movie = moviesres.json();
+    const director = directorres.json();
+    const acotor = acotorres.json();
+
+    console.log(movie, director, acotor);
+}
+
+getAlldatawithPromiseAll();
