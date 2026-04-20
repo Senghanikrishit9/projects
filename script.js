f39dadc0052a2339b8e9e78488725e430502355c
krishit senghani <krishitsenghani22@gmail.com>
2026-04-20 06:41:21 +0530

    object created

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-function Rectangle(name, width, height){
-    this.name = name;
-    this.width = width;
-    this.height = height;
+const rectangleprototypes = {
+    area : function(){
+        return this.width * this.height;
+    },
+    perimeter : function(){
+        return 2 * (this.width + this.height);
+    },
+    issquare : function(){
+        return this.width === this.height;
+    }
 }
 
-Rectangle.prototype.area = function() {
-    return this.width* this.height;
-}
-Rectangle.prototype.perimeter = function(){
-    return 2 *(this.width + this.height);
-}
-Rectangle.prototype.isSquare = function(){
-    return (this.width === this.height);
+function createRectangle(height,width){
+    return Object.create(rectangleprototypes,{
+        height : {
+            value : height
+        },
+
+        width : {
+            value : width
+        }
+    })
 }
 
-Rectangle.prototype.changeName = function(newName){
-    return (this.name = newName);
-}
-const rect  = new Rectangle('react', 10,10);
-
+const rect = createRectangle(10,20);
 console.log(rect);
-console.log(rect.area());
-console.log(rect.perimeter());
-console.log(rect.isSquare());
-console.log(rect.changeName('krishit'));
-
-
 
