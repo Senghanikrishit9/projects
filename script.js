94adfd0656c596880b0ee6948f65538532b748dc
krishit senghani <krishitsenghani22@gmail.com>
2026-04-22 17:06:37 +0530

    Prototypical inheritance & call

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-const rectangleprototypes = {
-    area : function(){
-        return this.width * this.height;
-    },
-    perimeter : function(){
-        return 2 * (this.width + this.height);
-    },
-    issquare : function(){
-        return this.width === this.height;
-    }
+function Shape(name) {
+    this.name = name;
+};
+
+Shape.prototype.logname = function () {
+    console.log(`shape Name : ${this.name}`);
+
 }
 
-function createRectangle(height,width){
-    return Object.create(rectangleprototypes,{
-        height : {
-            value : height
-        },
+function Rectangle(name, height, width) {
+    Shape.call(this, name);
 
-        width : {
-            value : width
-        }
-    })
+    this.height = height;
+    this.width = width;
+};
+// Inhertis shape prototype
+Rectangle.prototype = Object.create(Shape.prototype);
+
+Rectangle.prototype.logname = function(){
+    console.log(`rectangle Name : ${this.name}`);
+    
 }
 
-const rect = createRectangle(10,20);
-console.log(rect);
+function circle(name, radius) {
+    Shape.call(this, name);
+    this.radius = radius;
+};
+
+const rect = new Rectangle("rectangle", 40, 40);
+// console.log(rect);
+
+const circle1 = new circle("krishit", 20);
+// console.log(circle1);
+
+rect.logname();
 
