4ec8e9c5a9a6fe197e1c5c1ecc463bf2d7760201
krishit senghani <krishitsenghani22@gmail.com>
2026-04-25 11:32:02 +0530

    Classes

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-function player(name) {
- this.name = name,
- this.lvl = 1,
- this.points = 0;
- }
-
-player.prototype.gainxp = function (xp){
-    this.points += xp;
-
-    if(this.points >= 10){
-        this.lvl++;
-        this.points -= 10;
+class Rectangle{
+    constructor(name,height,width){
+        this.name = name,
+        this.height = height,
+        this.width = width
     }
- }
+    area (){
+        return this.height * this.width
+    }
 
- player.prototype.describe = function() {
-    return`${this.name} is level ${this.lvl} with ${this.points} experience points`
- }
-
-
- const player1 = new player('krish');
- const player2 = new player('krishit');
-
-
- player1.gainxp(4);
- player1.gainxp(7);
- player1.gainxp(2);
- player1.gainxp(9);
- 
- player2.gainxp(3);
- player2.gainxp(5);
- player2.gainxp(8);
- 
- console.log(player1.describe());
- console.log(player2.describe());
+    perimeter(){
+        return (this.width + this.height)
+    }
+    isSqaure(){
+        return this.height === this.width
+    }
+}
+const square = new Rectangle("square",20,20)
+console.log(square.area());
+console.log(square.perimeter());
+console.log(square.isSqaure());
 
 
- 
+
