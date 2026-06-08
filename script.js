892cd734c4611c68de2e40dd686755f812f4f72a
krishit senghani <krishitsenghani22@gmail.com>
2026-06-08 16:14:26 +0530

    Added navbar

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-class Wallet {
-    constructor(){
-        this._balnace = 0;
-    }
- deposit(amount){
-    this._balnace += amount;
- }  
- 
- withdraw(amount){
-    this._balnace-= amount;
- }
- getBalance(){
-    return this._balnace;
- }
+let amount = Number(prompt("What is your final amount"))
 
+if(amount>0 && amount <=5000){
+    console.log("you cannot get any discount")
+}else if(amount>5001 && amount <=7000){
+    console.log("you can get 5% discount")
+}else if(amount>7001 && amount<=9000){
+    console.log("you can get 10% discount")
+}else{
+    console.log("you can get  20% discount");
 }
-
-const Wallet = new wallet();
-Wallet.deposit(300);
-Wallet.withdraw(50);
-console.log(Wallet.getBalance());
